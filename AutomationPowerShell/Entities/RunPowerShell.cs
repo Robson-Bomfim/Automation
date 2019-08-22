ef0ab323103cd9e4efec5b9192d1376dba915575
Robson Bomfim <robsombomfim@gmail.com>
2019-08-22 12:50:18 -0300

    Iniciando o projeto de automação

---
+++ AutomationPowerShell/Entities/RunPowerShell.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPowerShell.Entities
+{
+    class RunPowerShell
+    {
+        public string Command { private get; set; }
+        private Collection<PSObject> Results;
+
+        public RunPowerShell()
+        {
+        }
+
+        public RunPowerShell(string command)
+        {
+            Command = command;
+        }
+
+        public void ExecutePowerShell()
+        {
+            RunspaceConfiguration config = RunspaceConfiguration.Create();
+            // create Powershell runspace
+            using (Runspace runspace = RunspaceFactory.CreateRunspace(config))
+            {
+                //open it 
+                runspace.Open();
+
+                // create a pipeline and feed it the script text
+                Pipeline pipeline = runspace.CreatePipeline(Command);
+                //pipeline.Commands.AddScript(Command);
+                //"Start-Process powershell -Verb runAs"
+                //execute the script
+                Results = pipeline.Invoke();
+            }
+        }
+
+        public override string ToString()
+        {
+            // convert the script result into a single string
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in Results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ AutomationPowerShell/Program.cs
@@ -0,0 +1,24 @@
+﻿using AutomationPowerShell.Entities;
+using System;
+
+namespace AutomationPowerShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string scriptText = "Stop-WebAppPool -Name \"Portal_BackEnd2\"";
+                RunPowerShell ps = new RunPowerShell(scriptText);
+                ps.ExecutePowerShell();
+                Console.WriteLine(ps.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
